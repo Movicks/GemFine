8487f00f5e4dfdefcc284d1bd0c0d055a7f276d7
Movicks <codlabx@gmail.com>
2024-08-21 22:59:50 +0100

    bug fixing

--- src/routes/LogAndRegRoute.tsx
+++ src/routes/LogAndRegRoute.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom'
 
 const LogAndRegRoute = () => {
   return (
-      <div className='h-full'>
-        <div className='flex flex-col gap-3 p-2 items-center md:pt-[5rem] justify-center h-auto bg-transparent'>
-          <div className='w-full md:max-w-[30rem] md:p-4 md:shadow-custom py-2 md:rounded-2xl px-1 flex flex-col justify-center gap-3'>
+      <div className='h-full bg-white md:bg-transparent'>
+        <div className='flex flex-col gap-3 p-2 items-center md:pt-[5rem] justify-center h-auto md:h-full bg-white md:bg-transparent'>
+          <div className='w-full md:max-w-[30rem] md:p-4 md:shadow-custom py-2 md:rounded-2xl px-1 flex flex-col bg-white justify-center gap-3'>
             <AnimatedLinker />
             <Outlet/> 
           </div> 
