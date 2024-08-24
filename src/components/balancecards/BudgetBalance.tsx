1c859fbca4212242e51f0cacf144c2f5b7a7becb
Movicks <codlabx@gmail.com>
2024-08-25 00:56:57 +0100

    bug fixing and new features

--- src/components/balancecards/BudgetBalance.tsx
+++ src/components/balancecards/BudgetBalance.tsx
@@ -3,7 +3,15 @@ import BalanceCard from './BalanceCard';
 
 const BudgetBalance = () => {
   return (
-    <BalanceCard bgColor='bg-white' textColor='text-red-500' subText='text-gray-500' balanceTitle='Budgets' Balance='700'/>
+    <BalanceCard
+      bgColor='bg-white'
+      textColor='text-red-500'
+      subText='text-gray-500'
+      balanceTitle='Budgets'
+      Balance='700'
+      SbtnStyles=''
+      SbtnContent={``}
+    />
   )
 }
 
